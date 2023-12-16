4585ef528ce8cd513179e85dccf69fe8c780977b
van <van.boinov@gmail.com>
2023-12-17 01:37:39 +0700

    актуализировал чарты

--- helm-install.sh
+++ helm-install.sh
@@ -2,7 +2,7 @@
 
 export KEYCLOAK_POSTGRESQL_ADMIN_PASSWORD=postgres
 export KEYCLOAK_POSTGRESQL_PASSWORD=postgres
-export KEYCLOAK_ADMIN_PASSWORD=user
-export CLUSTER_URL=-your-namespace.openshiftapps.com
+export KEYCLOAK_ADMIN_PASSWORD=CtD4.YA8tMLFgrc
+export CLUSTER_URL=-vanbv-1-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com
 
-helm install list-keep ./ --set global.postgresql.auth.postgresPassword=$KEYCLOAK_POSTGRESQL_ADMIN_PASSWORD,global.postgresql.auth.password=$KEYCLOAK_POSTGRESQL_PASSWORD,keycloak.auth.adminPassword=$KEYCLOAK_ADMIN_PASSWORD,list-keep.env[0].name=SPRING_PROFILES_ACTIVE,list-keep.env[0].value=prod,list-keep.env[1].name=KEYCLOAK_URL,list-keep.env[1].value=http://list-keep-keycloak-http$CLUSTER_URL,list-keep-front.route.host=list-keep-front$CLUSTER_URL,keycloak.route.http.host=list-keep-keycloak-http$CLUSTER_URL,keycloak.route.https.host=list-keep-keycloak-https$CLUSTER_URL
+helm install list-keep ./ --set global.postgresql.auth.postgresPassword=$KEYCLOAK_POSTGRESQL_ADMIN_PASSWORD,global.postgresql.auth.password=$KEYCLOAK_POSTGRESQL_PASSWORD,keycloak.auth.adminPassword=$KEYCLOAK_ADMIN_PASSWORD,list-keep.env[0].name=SPRING_PROFILES_ACTIVE,list-keep.env[0].value=prod,list-keep.env[1].name=KEYCLOAK_URL,list-keep.env[1].value=http://list-keep-keycloak-http$CLUSTER_URL/realms/list-keep,list-keep-front.route.host=list-keep-front$CLUSTER_URL,keycloak.route.http.host=list-keep-keycloak-http$CLUSTER_URL,keycloak.route.https.host=list-keep-keycloak-https$CLUSTER_URL
