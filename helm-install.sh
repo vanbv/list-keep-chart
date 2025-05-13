eb36cc78c5c4b3ae552005b0effd20ff590305bd
1ven <van.boinov@gmail.com>
2025-05-14 03:47:38 +0700

    убрал дублирование переменных окружения для бэка из скрипта установки чартов

--- helm-install.sh
+++ helm-install.sh
@@ -5,4 +5,11 @@ export KEYCLOAK_POSTGRESQL_PASSWORD=postgres
 export KEYCLOAK_ADMIN_PASSWORD=user
 export CLUSTER_URL=-your-namespace.openshiftapps.com
 
-helm install list-keep ./ --set global.postgresql.auth.postgresPassword=$KEYCLOAK_POSTGRESQL_ADMIN_PASSWORD,global.postgresql.auth.password=$KEYCLOAK_POSTGRESQL_PASSWORD,keycloak.auth.adminPassword=$KEYCLOAK_ADMIN_PASSWORD,list-keep.env[0].name=SPRING_PROFILES_ACTIVE,list-keep.env[0].value=prod,list-keep.env[1].name=POSTGRES_PASSWORD,list-keep.env[1].valueFrom.secretKeyRef.name=list-keep-postgresql,list-keep.env[1].valueFrom.secretKeyRef.key=postgres-password,list-keep.env[2].name=KEYCLOAK_URL,list-keep.env[2].value=https://list-keep-keycloak$CLUSTER_URL/realms/list-keep,list-keep-front.route.host=list-keep-front$CLUSTER_URL,keycloak.route.host=list-keep-keycloak$CLUSTER_URL
+helm install list-keep ./ \
+  --set global.postgresql.auth.postgresPassword=$KEYCLOAK_POSTGRESQL_ADMIN_PASSWORD \
+  --set global.postgresql.auth.password=$KEYCLOAK_POSTGRESQL_PASSWORD \
+  --set keycloak.auth.adminPassword=$KEYCLOAK_ADMIN_PASSWORD \
+  --set list-keep.extraEnv[0].name="KEYCLOAK_URL" \
+  --set list-keep.extraEnv[0].value="https://list-keep-keycloak$CLUSTER_URL/realms/list-keep" \
+  --set list-keep-front.route.host=list-keep-front$CLUSTER_URL \
+  --set keycloak.route.host=list-keep-keycloak$CLUSTER_URL
