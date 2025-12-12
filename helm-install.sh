0ab96ba412066f09f79e1ff3165ed7676ba0eda2
1ven <van.boinov@gmail.com>
2025-12-12 19:04:30 +0700

    перешёл с чартов bitnami на чарты cloudpirates

--- helm-install.sh
+++ helm-install.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-export KEYCLOAK_POSTGRESQL_ADMIN_PASSWORD=postgres
-export KEYCLOAK_POSTGRESQL_PASSWORD=postgres
-export KEYCLOAK_ADMIN_PASSWORD=user
-export CLUSTER_URL=-your-namespace.openshiftapps.com
+export CLUSTER_URL=-vanbv-1-dev.apps.rm3.7wse.p1.openshiftapps.com
 
 helm install list-keep ./ \
-  --set global.postgresql.auth.postgresPassword=$KEYCLOAK_POSTGRESQL_ADMIN_PASSWORD \
-  --set global.postgresql.auth.password=$KEYCLOAK_POSTGRESQL_PASSWORD \
-  --set keycloak.auth.adminPassword=$KEYCLOAK_ADMIN_PASSWORD \
   --set list-keep.extraEnv[0].name="KEYCLOAK_URL" \
   --set list-keep.extraEnv[0].value="https://list-keep-keycloak$CLUSTER_URL/realms/list-keep" \
   --set list-keep-front.route.host=list-keep-front$CLUSTER_URL \
